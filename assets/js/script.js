2ce1d3e923edd4e7f32b31c80d49714303abb4f1
Chrlss <chrlss@yahoo.com>
2023-08-21 23:29:45 +0800

    -hide nav bar when scroll down. -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+var lastScrollTop;
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+const navbar = document.getElementById('navbar');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+window.addEventListener('scroll',function(){
+ 
+   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+   
+   if(scrollTop > lastScrollTop){ 
+     navbar.style.top='-80px';
+   }
+   else{
+     navbar.style.top='0';
+   }
+   lastScrollTop = scrollTop; 
+ });
