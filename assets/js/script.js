6a71e474245655b8bb4639aa7d30ecb28d331ed4
Chrlss <brucesamonte20@gmail.com>
2023-08-22 22:59:52 +0800

    -Back2topbuttoninfooter-

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var lastScrollTop;
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
+const back2Top =document.getElementById('back2Top');
 const navbar = document.getElementById('navbar');
 
 btn.addEventListener('click', () => {
@@ -20,3 +21,20 @@ window.addEventListener('scroll',function(){
    }
    lastScrollTop = scrollTop; 
  });
+ window.addEventListener("scroll", (e) => {
+  if(window.scrollY >= 400) {
+      
+      back2Top.classList.remove("hidden");
+      back2Top.classList.add("block");
+      
+  } else {
+      back2Top.classList.add("hidden");
+      back2Top.classList.remove("block");
+  }
+});
+
+ back2Top.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scroll({
+  top:0, left:0, behavior:"smooth"});
+  });
